18b40132f6ca035ecb7ca58bd8bf96c516fad5ff
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-02 23:02:53 +0530

    Add All Component Do Some Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import { Contact } from "@/components/Contact";
+import { Education } from "@/components/Education";
 import { Experience } from "@/components/Experience";
 import Hero from "@/components/Hero";
+import { Projects } from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
     <>
-      <h1>Page Setup</h1>
+      <Hero />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Education />
+      <Contact />
     </>
   );
 }
