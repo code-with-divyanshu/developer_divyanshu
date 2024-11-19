4b49f4ee99a0c9c56f78dd7adcfe646ed18635d6
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-19 23:00:48 +0530

    Modify Contact Us Component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,49 +2,50 @@
 import React from "react";
 import { BackgroundBeams } from "./ui/background-beams";
 import Button from "./Button";
+import { Input, Textarea } from "./Input";
 
 export function Contact() {
   return (
-    <div
+    <section
       id="contact_me"
-      className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased"
+      className="flex justify-center items-center relative flex-col"
     >
-      <div className="max-w-2xl lg:max-w-4xl lg:w-full mx-auto z-20">
-        <h1 className="relative z-10 text-2xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-purple-600  text-center font-sans font-bold">
+      <div className="max-w-[370px] lg:max-w-4xl sm:max-w-2xl flex flex-col justify-center items-center relative gap-3 w-full my-20">
+        <h1 className="relative text-2xl md:text-6xl my-4 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-purple-600  text-center font-sans font-bold">
           Contact Me
         </h1>
-        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+        <p className="lg:text[24px] text-[18px] text-center font-[600] text-gray-500">
           Feel free to reach out to me for any questions or opportunities!
         </p>
-        <div className="flex flex-col lg:flex-row gap-3 z-20">
-          <input
+        <div className="w-full flex flex-col lg:flex-row gap-3">
+          <Input
             type="text"
             placeholder="Enter Your Name"
-            className="p-4 rounded-lg border border-neutral-500 focus:ring-2 focus:ring-teal-500  w-full relative z-30 mt-4  bg-neutral-950 placeholder:text-neutral-400"
+            className="p-4 py-4 rounded-lg border border-neutral-500 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-400"
           />
-          <input
+          <Input
             type="email"
             placeholder="Enter Your Email"
-            className="p-4 rounded-lg border border-neutral-500 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-400"
+            className="p-4 py-4 rounded-lg border border-neutral-500 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-400"
           />
         </div>
-        <div className="flex flex-col lg:flex-row gap-3 z-20">
-          <input
+        <div className="w-full flex flex-col lg:flex-row gap-3">
+          <Input
             type="number"
             placeholder="Enter Your Number"
-            className="p-4 rounded-lg border border-neutral-500 focus:ring-2 focus:ring-teal-500  w-full relative z-30 mt-4  bg-neutral-950 placeholder:text-neutral-400"
+            className="p-4 py-4 rounded-lg border border-neutral-500 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-400"
           />
-          <input
+          <Input
             type="text"
             placeholder="Enter Your Subject"
-            className="p-4 rounded-lg border border-neutral-500 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-400"
+            className="p-4 py-4 rounded-lg border border-neutral-500 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-400"
           />
         </div>
-        <textarea
+        <Textarea
           rows={5}
           placeholder="Write Your Message"
           className="p-4 rounded-lg border border-neutral-500 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-400"
-        ></textarea>
+        ></Textarea>
         <Button
           type="submit"
           title="Submit"
@@ -52,6 +53,6 @@ export function Contact() {
         />
       </div>
       <BackgroundBeams />
-    </div>
+    </section>
   );
 }
