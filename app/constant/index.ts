c6ae7ec97f84c05863394c3207a20b87c123b867
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-19 23:00:07 +0530

    Add Some Data

--- app/constant/index.ts
+++ app/constant/index.ts
@@ -1,7 +1,6 @@
 // NAVIGATION
 export const NAV_LINKS = [
   { href: "/", key: "home", label: "Home" },
-  { href: "#about", key: "about", label: "About" },
   { href: "#experience", key: "experience", label: "Experience" },
   { href: "#skills", key: "skills", label: "Skills" },
   { href: "#projects", key: "projects ", label: "Projects " },
