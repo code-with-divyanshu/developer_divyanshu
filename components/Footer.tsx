22c99d0f2064903202825706a32987657553f9db
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-21 23:22:09 +0530

    Modify Footer Component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,13 +3,27 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-4 lg:p-24 md:p-16 p-6">
-      <div className="">
-        <Image src="/web-development.png" alt="Logo" width={80} height={80} />
+    <div className="mb-20">
+      <div className="flex sm:flex-row flex-col items-center justify-between lg:items-start lg:flex-row gap-4 lg:p-24 md:p-16 p-6">
+        <div className="">
+          <Image src="/web-development.png" alt="Logo" width={80} height={80} />
+        </div>
+        <div className="text-center">
+          <h2 className="text-purple-500 font-bold">Location</h2>
+          <p>Sarswati Vihar, E-Block</p>
+          <p>Dehradun, 248001</p>
+          <p>Uttarakhand, India</p>
+        </div>
+        <div className="text-center">
+          <h2 className="text-purple-500 font-bold">Contact</h2>
+          <p>Sarswati Vihar, E-Block</p>
+          <p>Dehradun, 248001</p>
+          <p>Uttarakhand, India</p>
+        </div>
       </div>
-      <div className="">2</div>
-      <div className="">3</div>
-      <div className="">4</div>
+      <div className="text-center">
+        <p>© 2024 Developer Divyanshu || All Copy Rights Reserved</p>
+      </div>
     </div>
   );
 };
