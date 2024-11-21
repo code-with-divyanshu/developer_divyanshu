b3fa4dd7c0cbb01cca356be74037459e32d0f888
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-21 23:21:43 +0530

    Modify Skills Component

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -11,7 +11,7 @@ const Skills = () => {
       className="flex justify-center items-center relative flex-col -z-10 my-10"
     >
       <div className="flex flex-col justify-center items-center relative gap-3 w-full my-20">
-        <h1 className="text-[32px] text-center font-[600] mt-[12px] text-purple-700 lg:text-[42ppxx] lg:mt-[20px]">
+        <h1 className="relative text-2xl md:text-6xl my-4 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-purple-600  text-center font-sans font-bold">
           Skills
         </h1>
         <p className="lg:text[24px] text-[18px] text-center font-[600] text-gray-500">
@@ -24,8 +24,8 @@ const Skills = () => {
               key={index}
               className="rounded-[22px] max-w-sm  bg-white dark:bg-zinc-900 flex flex-row"
             >
-              <div className="w-full max-w-[360px] bg-gray-800   rounded-[16px] py-[10px] px-[36px] lg:max-w-[400px] md:max-w-[700px] lg:py-[18] lg:p-[36]">
-                <h1 className="text-[28px] font-[600] mb-[20px] text-center text-text-gray-500">
+              <div className="w-full max-w-[360px] bg-gray-800  rounded-[16px] py-[10px] px-[36px] lg:max-w-[400px] md:max-w-[700px] lg:py-[18] lg:p-[36]">
+                <h1 className="text-[28px] font-[600] mb-[20px] text-center bg-clip-text text-transparent bg-gradient-to-b from-teal-500 to-purple-900">
                   {skill.title}
                 </h1>
                 <div className="flex flex-wrap justify-center gap-3 mb-5">
