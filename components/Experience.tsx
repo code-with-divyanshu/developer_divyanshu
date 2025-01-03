e73eff877d1a3dcf5de2499fead91520321bbce9
code-with-divyanshu <divyanshubartwal67@gmail.com>
2025-01-03 23:29:48 +0530

    Update Experience Section Data and Component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { Timeline } from "@/components/ui/timeline";
+import Link from "next/link";
 
 export function Experience() {
   const data = [
@@ -30,13 +31,15 @@ export function Experience() {
       content: (
         <div>
           <div className="grid grid-cols-2 gap-4">
-            <Image
-              src="/mega_blog.png"
-              alt="Mega Blog App"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
+            <Link href="https://mega-blog-app.netlify.app">
+              <Image
+                src="/mega_blog.png"
+                alt="Mega Blog App"
+                width={500}
+                height={500}
+                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+              />
+            </Link>
             <Image
               src="/music_academy.png"
               alt="Music Academy"
